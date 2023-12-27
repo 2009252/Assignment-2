7449cd1dbe375c10835be6d8cb8b8db5caf61e55
2009252@student.uwtsd.ac.uk <2009252@student.uwtsd.ac.uk>
2023-12-27 13:23:27 +0000

    4

--- New folder/Domain search.py
+++ New folder/Domain search.py
@@ -1,9 +1,15 @@
 import requests
 
 def domain_search(domain_name):
-    api_url = f"https://api.domainsdb.info/v1/domains/search?domain={domain_name}:checkAvailability"
+    api_url = f"https://domains.google.com/v2alpha1/registrations/{domain_name}:checkAvailability"
+
 
     try:
         response = requests.get(api_url)
         response.raise_for_status()
     except requests.exceptions.HTTPError as errh:
+        print(f"HTTP Error: {errh}")
+        return False
+    except requests.exceptions.ConnectionError as errc:
+        print(f"Error Connecting: {errc}")
+        return False
