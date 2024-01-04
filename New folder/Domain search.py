44c5fc173c8d0c27d77fd8f02b823e3e2bf424e7
2009252@student.uwtsd.ac.uk <2009252@student.uwtsd.ac.uk>
2024-01-04 18:07:07 +0000

    8

--- New folder/Domain search.py
+++ New folder/Domain search.py
@@ -4,22 +4,7 @@ import requests
 def check_domain_availability(domain_name):
     api_url = f"https://api.domainsdb.info/v1/domains/search?domain={domain_name}"
 
-    try:
-        response = requests.get(api_url)
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as errh:
-        print(f"HTTP Error: {errh}")
-        return False
-    except requests.exceptions.ConnectionError as errc:
-        print(f"Error Connecting: {errc}")
-        return False
-    except requests.exceptions.Timeout as errt:
-        print(f"Timeout Error: {errt}")
-        return False
-    except requests.exceptions.RequestException as err:
-        print(f"Error: {err}")
-        return False
-    try:
+       try:
         response = requests.get(api_url)
         data = response.json()
 
