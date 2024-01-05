c56bba08d77b2961740a64bfeec1968a9e0527d2
2009252@student.uwtsd.ac.uk <2009252@student.uwtsd.ac.uk>
2024-01-05 10:07:47 +0000

    Final

--- New folder/Domain search.py
+++ New folder/Domain search.py
@@ -1,26 +1,24 @@
 import requests
 
-
 def check_domain_availability(domain_name):
     api_url = f"https://api.domainsdb.info/v1/domains/search?domain={domain_name}"
 
+    response = requests.get(api_url)
 
-    try:
-        response = requests.get(api_url)
-        if response.status_code == 200:
-         data = response.json()
-
+    if response.status_code == 200:
+        data = response.json()
         if data['total'] > 0:
             print(f"Sorry, {domain_name} is not available.")
+            print("-------------------------------------------------------------")
+            print("There are", data['total'], "Domains with this word used")
+            print("-------------------------------------------------------------")
             print("First 20 results:")
+            print("-------------------------------------------------------------")
             for result in data['domains'][:20]:
                 print(result['domain'])
-            else:
-                print(f"{domain_name} is available!")
-        else:
-            print(f"Error: Unable to check availability for {domain_name}. Status code: {response.status_code}")
+    else:
+        print(f"Domain Name is availabile for: {domain_name}. Status code: The domain can not be found: {response.status_code}")
 
-
-    if __name__ == "__main__":
+if __name__ == "__main__":
     user_input = input("Enter a domain name to check its availability: ")
     check_domain_availability(user_input)
